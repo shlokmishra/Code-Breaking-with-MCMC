76d88b560591cbd4e654f33b927e2e384454938d
Shlok Mishra <37180838+shlokmishra@users.noreply.github.com>
2022-12-23 20:15:59 +0530

    post 23rd dec meet changes

--- code-breaking-using-metropolis.R
+++ code-breaking-using-metropolis.R
@@ -1,12 +1,14 @@
 source("function.R")
 
 # Choose a value of n which will be the number of iterations
-n <- 1000
+n <- 100
 
 # Choose the string which you would like to encrypt and then decrypt using metropolis algorithm
 giventext <- "this is a sample peice of text and i am really glad that this code can easily crack it"
 
 
-decoded_text_best <- encrypt_decrypt_using_metropolis(giventext)
+decoded_text_best <- encrypt_decrypt_using_metropolis(giventext, n)
 paste("The best decoded text reached in" , n , "iterations is: '")
 decoded_text_best
+
+
