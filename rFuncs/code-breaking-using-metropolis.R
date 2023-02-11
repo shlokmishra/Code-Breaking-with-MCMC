4fb779570c5b8ec10820998f73c6a96e052035c8
Shlok Mishra <37180838+shlokmishra@users.noreply.github.com>
2023-02-11 16:51:17 +0530

    trying a new prop dist

--- rFuncs/code-breaking-using-metropolis.R
+++ rFuncs/code-breaking-using-metropolis.R
@@ -1,5 +1,5 @@
   source("rFuncs/metropFuncs.R")
-
+  source("rFuncs/new-prop-dist.R")
   
   
   # Choose a value of n which will be the number of iterations
@@ -20,17 +20,18 @@
   # Using both Regular and Modified Metropolis Algorithm functions to decrypt 
   # the given text in the given number of iterations
   decodedBestReg <- decryptMetropReg(cipheredText, n)
-  decodedBestModified <- decryptMetropModified(cipheredText, n)
+  # decodedBestModified <- decryptMetropModified(cipheredText, n)
+  decodedBestModifiedTest <- decryptMetropModifiedTest(cipheredText, n)
   
   # Printing the decrypted text which has highest similarity score
   paste("The best decoded text reached in" , n , "iterations is: '")
   decodedBestReg[[1]]
-  decodedBestModified[[1]]
+  decodedBestModifiedTest[[1]]
   
   # Visualising the change in similarity score throughout the process
   plot(decodedBestReg[[2]], ylim = range(c(decodedBestReg[[2]], logLik(plainText))),
     xlim = c(1, n), type = 'l')
-  lines(decodedBestModified[[2]], col = "green")
+  lines(decodedBestModifiedTest[[2]], col = "green")
   abline(h=logLik(plainText), col = "red")
   
 # save(decodedBestModified, decodedBestReg, n, plainText, file = "data50kIter.Rdata")
