16ce023693215a9fea7fd6b4607039db81e4017e
Shlok Mishra <37180838+shlokmishra@users.noreply.github.com>
2023-03-13 23:19:48 +0530

    multiTry implementation

--- rFuncs/code-breaking-using-metropolis.R
+++ rFuncs/code-breaking-using-metropolis.R
@@ -3,7 +3,7 @@
   
   
   # Choose a value of n which will be the number of iterations
-  n <- 10000
+  n <- 50
   
   
   # Choose the string which you would like to encrypt and then decrypt using metropolis algorithm
@@ -22,21 +22,21 @@
   decodedBestReg <- decryptMetropReg(cipheredText, n)
   decodedBestBarker <- decryptMetropBarker(cipheredText, n)
   decodedBestLit <- decryptMetropLit(cipheredText, n)
-  
+  decodedBestMultiTry <- decryptMetropMultiTry(cipheredText,50 ,n)
   # Printing the decrypted text which has highest similarity score
   paste("The best decoded text reached in" , n , "iterations is: '")
   decodedBestReg[[1]]
   decodedBestBarker[[1]]
   decodedBestLit[[1]]
-  
+  decodedBestMultiTry[[1]]
   # Visualising the change in similarity score throughout the process
   plot(decodedBestReg[[2]], ylim = range(c(decodedBestReg[[2]], logLik(plainText))),
     xlim = c(1, n), type = 'l', xlab = "Log-Likelihoods", ylab = "Number of Iterations", main = "Comparing different MH algorithms")
   lines(decodedBestBarker[[2]], col = "red")
   lines(decodedBestLit[[2]], col = "blue")
-  
+  lines(decodedBestMultiTry[[2]], col = "purple")
   abline(h=logLik(plainText), col = "green")
-  legend("bottomright", legend = c("Reg", "Barker", "Lit"),lty = c(1,1,1) , col = c("black", "red", "blue")) 
+  legend("bottomright", legend = c("Reg", "Barker", "Lit", "MultiTry"),lty = c(1,1,1,1) , col = c("black", "red", "blue", "purple")) 
   
   # Uncomment the next line to save your results
   # save(decodedBestBarker,decodedBestLit, decodedBestReg, n, plainText, file = "data/10k-Iter.Rdata")
--- rFuncs/multiTryMetrop.R
+++ rFuncs/multiTryMetrop.R
@@ -1,14 +1,98 @@
 source("rFuncs/metropFuncs.R")
-n <- 50
+N <- 50
 
-neighbourSubset <- sample(1:326, size = n)
-score_1 <- numeric(length = n)
-score_2 <- numeric(length = n-1)
+samplingMultiTry <- function(givenCipher, N){
+  neighbourSubset_1 <- sample(2:326, size = N)
+  score_1 <- numeric(length = N)
+  score_2 <- numeric(length = (N-1))
+  
+  for(k in 1:N){
+    i <- allProposal[neighbourSubset_1[k], 1]
+    j <- allProposal[neighbourSubset_1[k], 2]
+    tempCipher <- swapIndicies(givenCipher,i,j)
+    score_1[k] <-  logLik(decodeText(cipheredText, tempCipher))
+  }
+  
+  proposal <- sample(neighbourSubset_1, size = 1, prob = exp(score_1)/sum(exp(score_1)))
+  indicies_to_swap <- allProposal[proposal, 1:2]
+  propCipher <- swapIndicies(givenCipher, indicies_to_swap[1], indicies_to_swap[2])
+  neighbourSubset_2 <- sample(2:326, size = N-1)
+  
+  for(k in 1:(N-1)){
+    i <- allProposal[neighbourSubset_2[k], 1]
+    j <- allProposal[neighbourSubset_2[k], 2]
+    tempCipher <- swapIndicies(propCipher,i,j)
+    score_2[k] <-  logLik(decodeText(cipheredText, tempCipher))
+  }
+  propDecoded <- decodeText(cipheredText,
+                            cipher = propCipher)
+  currDecoded <- decodeText(cipheredText,
+                            cipher = currCipher)
+  
+  propLogLik <- logLik(propDecoded)
+  currLogLik <- logLik(currDecoded)
+  
+  num <- sum(exp(score_1))
+  den <- sum(exp(score_2)) + exp(logLik(decodeText(cipheredText, givenCipher)))
+  
+  return(list((propCipher), c(num, den)))
+}
 
-for(k in 1:n){
-  i <- allProposal[neighbourSubset[k], 1]
-  j <- allProposal[neighbourSubset[k], 2]
-  tempCipher <- swapIndicies(givenCipher,i,j)
-  score_1[k] <-  logLik(decodeText(cipheredText, tempCipher))
+decryptMetropMultiTry <- function(cipheredText, N, n){
+  currCipher <- startingCipher
+  i <- 0
+  currDecoded <- decodeText(cipheredText,
+                            cipher = currCipher)
+  
+  currLogLik <- logLik(currDecoded)
+  max_score <- currLogLik
+  bestCipher <- currCipher
+  
+  similar <- numeric(length = n)
+  similar[1] <- currLogLik
+  for (iter in 2:n) {
+    
+    foo <- samplingMultiTry(currCipher, N)
+    propCipher <- foo[[1]]
+    num <- foo[[2]][1]
+    den <- foo[[2]][2]
+    propDecoded <- decodeText(cipheredText,
+                              cipher = propCipher)
+    currDecoded <- decodeText(cipheredText,
+                              cipher = currCipher)
+    
+    propLogLik <- logLik(propDecoded)
+    currLogLik <- logLik(currDecoded)
+    
+    acceptProb <- min(1, num/den)
+    
+    accept <- sample(c(TRUE, FALSE),
+                     size=1,
+                     prob = c(acceptProb,
+                              1-acceptProb))
+    
+    if (accept) {
+      currCipher <- propCipher
+      currLogLik <- propLogLik
+      i <- i + 1
+    }
+    
+    similar[iter] <- currLogLik
+    
+    if(currLogLik > max_score){
+      max_score <- currLogLik
+      bestCipher <- currCipher
+    }
+    if(iter %% (n/100) == 0){
+      x <- iter*100/n
+      print(paste(x, "% Completed"))
+    }
+    
+    
+  }
+  bestDecoded <- decodeText(cipheredText,
+                            cipher = bestCipher)
+  
+  return(list(bestDecoded, similar))
 }
 
